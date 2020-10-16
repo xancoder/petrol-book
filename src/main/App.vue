f3b63cd07b27d7259a8a2988f2917d9efb23ef00
Alexander Sommer <xancoder@gmail.com>
2020-10-17 00:09:33 +0200

    refactor(app): add vuetify layout components
    * app, app-bar and main component to app
    * container, row, col component to landing-page

--- src/main/App.vue
+++ src/main/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+const {label} = require('../../package')
+
 export default {
-  name: 'app'
+  components: {},
+  name: 'app',
+  data: () => ({
+    title: label
+  })
 }
 </script>
