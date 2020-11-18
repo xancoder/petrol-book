cd17c866a3ed858dd18dfe53c4c6fea70669c035
Alexander Sommer <xancoder@gmail.com>
2020-11-18 12:39:51 +0100

    feat(app): add div container due to vue js template root element

--- src/main/components/LandingPage.vue
+++ src/main/components/LandingPage.vue
@@ -1,194 +1,196 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <v-file-input
-        v-model="databaseFile"
-        v-on:change="loadDatabase()"
-        v-on:click:clear="resetDatabase()"
-        show-size
-        truncate-length="42"
-        prepend-icon="mdi-database"
-        accept=".json"
-      ></v-file-input>
-    </v-col>
-    <v-col cols="12">
-      <v-data-table
-        :headers="tableHeaders"
-        :items="workingData.fuelingOperations"
-        :sort-by="['date']"
-        :sort-desc="[true]"
-        class="elevation-1"
-      >
-        <template v-slot:top>
-          <v-toolbar flat>
-            <v-toolbar-title>Fueling Operations</v-toolbar-title>
-            <v-divider class="mx-4" inset vertical></v-divider>
-            <v-spacer></v-spacer>
-            <v-dialog v-model="tableDialog" max-width="500px">
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn
-                  v-bind="attrs"
-                  v-on="on"
-                  class="mb-2"
-                  color="primary"
-                  tile
-                  :disabled="!databaseFile"
-                >
-                  <v-icon>mdi-plus</v-icon>
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title>
-                  <span class="headline">{{ formTitle }}</span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container>
-                    <v-row>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-menu
-                          v-model="menuDate"
-                          :close-on-content-click="false"
-                          :nudge-right="40"
-                          min-width="290px"
-                          offset-y
-                          transition="scale-transition"
-                        >
-                          <template v-slot:activator="{ on, attrs }">
-                            <v-text-field
+  <diV>
+    <v-row>
+      <v-col cols="12">
+        <v-file-input
+          v-model="databaseFile"
+          v-on:change="loadDatabase()"
+          v-on:click:clear="resetDatabase()"
+          show-size
+          truncate-length="42"
+          prepend-icon="mdi-database"
+          accept=".json"
+        ></v-file-input>
+      </v-col>
+      <v-col cols="12">
+        <v-data-table
+          :headers="tableHeaders"
+          :items="workingData.fuelingOperations"
+          :sort-by="['date']"
+          :sort-desc="[true]"
+          class="elevation-1"
+        >
+          <template v-slot:top>
+            <v-toolbar flat>
+              <v-toolbar-title>Fueling Operations</v-toolbar-title>
+              <v-divider class="mx-4" inset vertical></v-divider>
+              <v-spacer></v-spacer>
+              <v-dialog v-model="tableDialog" max-width="500px">
+                <template v-slot:activator="{ on, attrs }">
+                  <v-btn
+                    v-bind="attrs"
+                    v-on="on"
+                    class="mb-2"
+                    color="primary"
+                    tile
+                    :disabled="!databaseFile"
+                  >
+                    <v-icon>mdi-plus</v-icon>
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="headline">{{ formTitle }}</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-menu
+                            v-model="menuDate"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            min-width="290px"
+                            offset-y
+                            transition="scale-transition"
+                          >
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field
+                                v-model="tableEditedItem.date"
+                                v-bind="attrs"
+                                v-on="on"
+                                label="Date"
+                                prepend-icon="mdi-calendar"
+                                readonly
+                              ></v-text-field>
+                            </template>
+                            <v-date-picker
                               v-model="tableEditedItem.date"
-                              v-bind="attrs"
-                              v-on="on"
-                              label="Date"
-                              prepend-icon="mdi-calendar"
-                              readonly
-                            ></v-text-field>
-                          </template>
-                          <v-date-picker
-                            v-model="tableEditedItem.date"
-                            @input="menuDate = false"
-                          ></v-date-picker>
-                        </v-menu>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-menu
-                          ref="menu"
-                          v-model="menuTime"
-                          :close-on-content-click="false"
-                          :nudge-right="40"
-                          :return-value.sync="tableEditedItem.time"
-                          min-width="290px"
-                          offset-y
-                          transition="scale-transition"
-                        >
-                          <template v-slot:activator="{ on, attrs }">
-                            <v-text-field
+                              @input="menuDate = false"
+                            ></v-date-picker>
+                          </v-menu>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-menu
+                            ref="menu"
+                            v-model="menuTime"
+                            :close-on-content-click="false"
+                            :nudge-right="40"
+                            :return-value.sync="tableEditedItem.time"
+                            min-width="290px"
+                            offset-y
+                            transition="scale-transition"
+                          >
+                            <template v-slot:activator="{ on, attrs }">
+                              <v-text-field
+                                v-model="tableEditedItem.time"
+                                v-bind="attrs"
+                                v-on="on"
+                                label="Time"
+                                prepend-icon="mdi-clock-time-four-outline"
+                                readonly
+                              ></v-text-field>
+                            </template>
+                            <v-time-picker
+                              v-if="menuTime"
                               v-model="tableEditedItem.time"
-                              v-bind="attrs"
-                              v-on="on"
-                              label="Time"
-                              prepend-icon="mdi-clock-time-four-outline"
-                              readonly
-                            ></v-text-field>
-                          </template>
-                          <v-time-picker
-                            v-if="menuTime"
-                            v-model="tableEditedItem.time"
-                            format="24hr"
-                            full-width
-                            @click:minute="$refs.menu.save(tableEditedItem.time)"
-                          ></v-time-picker>
-                        </v-menu>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-combobox
-                          v-model="tableEditedItem.petrolStation"
-                          :items="petrolStations"
-                          label="Petrol Station"
-                        ></v-combobox>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-combobox
-                          v-model="tableEditedItem.petrolType"
-                          :items="petrolTypes"
-                          label="Petrol Type"
-                        ></v-combobox>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="tableEditedItem.costs"
-                          label="Costs"
-                          type="number"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="tableEditedItem.liquid"
-                          label="Liquid"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="tableEditedItem.distance"
-                          label="Distance"
-                        ></v-text-field>
-                      </v-col>
-                      <v-col cols="12" sm="6" md="4">
-                        <v-text-field
-                          v-model="tableEditedItem.mileage"
-                          label="Mileage"
-                        ></v-text-field>
-                      </v-col>
-                    </v-row>
-                  </v-container>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn
-                    color="primary darken-1"
-                    text
-                    @click="close"
-                  >
-                    Cancel
-                  </v-btn>
-                  <v-btn
-                    color="primary darken-1"
-                    text
-                    @click="save"
-                  >
-                    Save
-                  </v-btn>
-                </v-card-actions>
-              </v-card>
-            </v-dialog>
-          </v-toolbar>
-        </template>
-        <template v-slot:item.actions="{ item }">
-          <v-icon
-            class="mr-2"
-            small
-            @click="editItem(item)"
-          >
-            mdi-pencil
-          </v-icon>
-          <v-icon
-            small
-            @click="deleteItem(item)"
-          >
-            mdi-delete
-          </v-icon>
-        </template>
-        <template v-slot:item.ppl="{ item }">
-          {{ (item.costs / item.liquid).toFixed(3) }}
-        </template>
-        <template v-slot:item.l100="{ item }">
-          {{ ((item.liquid * 100) / item.distance).toFixed(2) }}
-        </template>
-        <template v-slot:item.p100="{ item }">
-          {{ ((item.costs / item.liquid) * ((item.liquid * 100) / item.distance)).toFixed(2) }}
-        </template>
-      </v-data-table>
-    </v-col>
-  </v-row>
+                              format="24hr"
+                              full-width
+                              @click:minute="$refs.menu.save(tableEditedItem.time)"
+                            ></v-time-picker>
+                          </v-menu>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-combobox
+                            v-model="tableEditedItem.petrolStation"
+                            :items="petrolStations"
+                            label="Petrol Station"
+                          ></v-combobox>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-combobox
+                            v-model="tableEditedItem.petrolType"
+                            :items="petrolTypes"
+                            label="Petrol Type"
+                          ></v-combobox>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="tableEditedItem.costs"
+                            label="Costs"
+                            type="number"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="tableEditedItem.liquid"
+                            label="Liquid"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="tableEditedItem.distance"
+                            label="Distance"
+                          ></v-text-field>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            v-model="tableEditedItem.mileage"
+                            label="Mileage"
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="primary darken-1"
+                      text
+                      @click="close"
+                    >
+                      Cancel
+                    </v-btn>
+                    <v-btn
+                      color="primary darken-1"
+                      text
+                      @click="save"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-toolbar>
+          </template>
+          <template v-slot:item.actions="{ item }">
+            <v-icon
+              class="mr-2"
+              small
+              @click="editItem(item)"
+            >
+              mdi-pencil
+            </v-icon>
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
+              mdi-delete
+            </v-icon>
+          </template>
+          <template v-slot:item.ppl="{ item }">
+            {{ (item.costs / item.liquid).toFixed(3) }}
+          </template>
+          <template v-slot:item.l100="{ item }">
+            {{ ((item.liquid * 100) / item.distance).toFixed(2) }}
+          </template>
+          <template v-slot:item.p100="{ item }">
+            {{ ((item.costs / item.liquid) * ((item.liquid * 100) / item.distance)).toFixed(2) }}
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+  </diV>
 </template>
 
 <script>
